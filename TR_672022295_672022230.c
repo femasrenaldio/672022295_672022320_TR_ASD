ec9e2867f6be7a26d062fca9e141288839d96d3e
femasrenaldio <130587630+femasrenaldio@users.noreply.github.com>
2023-04-13 09:34:14 +0700

    Update TR_672022295_672022230.c

--- TR_672022295_672022230.c
+++ TR_672022295_672022230.c
@@ -1,1 +1,322 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <conio.h>
+#include <stdbool.h>
 
+
+struct dealer{
+    char kode[50];
+    char harga[50], jumlah[50];
+    char nama_mobil[50], jenis[50];
+};
+
+struct history{
+    char riwayat[100];
+};
+
+struct dealer dealer[100];
+struct history riwayat[100];
+
+void gotoxy();
+void login();
+void menu();
+void tambahDatabase();
+void tampilDatabase();
+void hapusDatabase();
+void edit();
+void cari();
+void urut();
+void keluar();
+
+int banyakdata, banyaktambahdata, i, j, k;
+int riwayad, found, total;
+int arr;
+int n=5;
+
+char rriwayat[100][100];
+
+int main(){
+    login();
+	//menu();
+}
+
+// Memberi nilai gotoxy
+void gotoxy(int x, int y)
+{
+	HANDLE hConsoleOutput;
+	COORD dwCursorPosition;
+	dwCursorPosition.X = x;
+	dwCursorPosition.Y = y;
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(hConsoleOutput,dwCursorPosition);
+}
+
+
+
+// Tampil Data Base
+void tampilDatabase(){
+    system("cls");
+    gotoxy(10,3);
+    Sleep(1000);
+    printf("+ ========================================================================================================= +");
+    gotoxy(60,5);
+    Sleep(1000);
+    printf("DATABASE");
+    gotoxy(40,6);
+    Sleep(1000);
+    printf("=== MENAMPILKAN ISI DATABASE DEALER MOBIL SAKIT KEPALA ===");
+    gotoxy(10,8);
+    Sleep(1000);
+    printf("+ ========================================================================================================= +");
+
+    gotoxy(10,10);
+    printf("|Kode Mobil");
+    gotoxy(33,10);
+    printf("|Nama Mobil");
+    gotoxy(56,10);
+    printf("|Jenis Mobil");
+    gotoxy(79,10);
+    printf("|Jumlah Mobil");
+    gotoxy(102,10);
+    printf("|Harga Mobil");
+
+    for(i=0; i < banyakdata; i++){
+        gotoxy(10,11+i+1);
+        printf("|%s", dealer[i].kode);
+        gotoxy(33, 11+i+1);
+        printf("|%s", dealer[i].nama_mobil);
+        gotoxy(56, 11+i+1);
+        printf("|%s", dealer[i].jenis);
+        gotoxy(79, 11+i+1);
+        printf("|%s", dealer[i].jumlah);
+        gotoxy(102, 11+i+1);
+        printf("|%s", dealer[i].harga);
+        printf("\n");
+    }
+    gotoxy(10,20);
+    printf("+ ========================================================================================================== +");
+    strcpy(rriwayat[riwayad], "ADMIN MENAMPILKAN DATABASE\n");
+    riwayad++;
+
+    gotoxy(11,30);printf("\nKlik Apa Saja Untuk Melanjutkan....");
+    getch();
+    menu();
+}
+
+//tambah data base
+void tambahDatabase(){
+    int b;
+    char kode[100];
+
+    system("cls");
+    balik:
+    gotoxy(30, 3);
+    Sleep(1000);
+    printf("+ ================================================= +");
+    gotoxy(37, 5);
+    Sleep(1000);
+    printf("                 DATABASE                            ");
+    gotoxy(37, 6);
+    Sleep(1000);
+    printf("    === MENAMBAHKAN ISI DATABASE ===        ");
+    gotoxy(30, 8);
+    Sleep(1000);
+    printf("+ ================================================= +");
+    gotoxy(30, 11);
+    printf("+ ================================================= +");
+    gotoxy(39, 9);
+    printf("DATA MAKSIMUM YANG BISA DIINPUT : %d", n-banyakdata);
+    gotoxy(39, 10);
+    printf("BANYAK DATA YANG INGIN DIINPUT : ");
+    scanf("%d", &banyaktambahdata);
+    int c = 0;
+
+    if(banyaktambahdata <=n-banyakdata && banyaktambahdata >= 1){
+        for(b = 0; b < banyaktambahdata; b++){
+
+            printf(" ");
+            gotoxy(32, 13+(b*6));
+            printf("\nData Dealer Mobil Ke-%d\n", b + 1);
+
+            gotoxy(32, 14+(b*6));
+            printf("Kode Mobil\t = ");
+            fflush(stdin);
+            scanf("%s", &dealer[banyakdata].kode);
+            for(j = 0; j < banyakdata; j++){
+                if(strcmp(dealer[j].kode, dealer[banyakdata].kode)==0){
+                    gotoxy(33,15+(j*b*6));
+                    printf("\n Kode Sudah Ada\n\n");
+                    gotoxy(35,17);
+                    printf("Tekan Apa Saja Untuk Melanjutkan....");
+                    getch();
+                    system("cls");
+                    goto balik;
+                }
+            }
+            gotoxy(32, 15+(b*6));
+            printf("Nama Mobil\t = ");
+            fflush(stdin);
+            scanf("%s", &dealer[banyakdata].nama_mobil);
+
+            gotoxy(32, 16+(b*6));
+            printf("Jenis Mobil\t = ");
+            fflush(stdin);
+            scanf("%s", &dealer[banyakdata].jenis);
+
+            gotoxy(32, 17+(b*6));
+            printf("Jumlah Mobil\t = ");
+            fflush(stdin);
+            scanf("%s", &dealer[banyakdata].jumlah);
+
+            gotoxy(32, 18+(b*6));
+            printf("Harga Mobil\t = ");
+            fflush(stdin);
+            scanf("%s", &dealer[banyakdata].harga);
+            banyakdata++;
+        }
+
+        strcpy(rriwayat[riwayad], "ADMIN MENAMBAH DATABASE\n");
+        riwayad++;
+        strcpy(dealer[i].kode, dealer[i].kode);
+        i++;
+
+        printf("\nKlik terserah untuk lanjut~ ");
+        getch();
+        menu();
+    }else{
+        gotoxy(72,10);printf(" Maksimal %d Cuyyy", n-banyakdata);
+        getch();
+        tambahDatabase();
+    }
+}
+
+void hapusDatabase()
+{
+    char kode[10], ans, i, j;
+
+    system("cls");
+    gotoxy(30,3);Sleep(1000);printf("+ ================================================= +");
+    gotoxy(30,5);Sleep(1000);printf("                      DATABASE                      ");
+    gotoxy(30,6);Sleep(1000);printf("         === MENGHAPUS ISI DATABASE ===    ");
+    gotoxy(30,8);Sleep(1000);printf("+ ================================================= +");
+    gotoxy(30,9);printf("      Masukkan kode mobil yang ingin dihapus :       ");
+    gotoxy(30,10);printf("+ ================================================= +");
+    gotoxy(78,9);scanf("%s", &kode);
+
+    if (banyakdata > 0) {
+	    	for(i = 0; i < banyakdata; i++)
+	    {
+	        if(strcmp(kode, dealer[i].kode) == 0)
+	        {
+	            gotoxy(32, 12);
+	            printf(":: Data Dealer Mobil ::");
+	            gotoxy(32, 13);
+	            printf(":: Kode Mobil\t = %s",dealer[i].kode);
+	            gotoxy(32, 14);
+	            printf(":: Nama Mobil\t = %s",dealer[i].nama_mobil);
+	            gotoxy(32, 15);
+	            printf(":: Jenis Mobil\t = %s",dealer[i].jenis);
+	            gotoxy(32, 16);
+	            printf(":: Jumlah Mobil\t = %s", dealer[i].jumlah);
+	            gotoxy(32, 17);
+	            printf(":: Harga Mobil\t = %s", dealer[i].harga);
+	            gotoxy(32, 19);
+	            printf("Yakin ingin menghapus data ini?[y/n] ");
+	            scanf("%s", &ans);
+
+	            if(ans == 'y' ||  ans == 'Y')
+	            {
+	                for(j = i; j <= banyakdata; j++)
+	                {
+	                    dealer[j] = dealer[j+1];
+	                }
+	                banyakdata--;
+	                strcpy(rriwayat[riwayad], "ADMIN MENGHAPUS DATABASE\n");
+       				riwayad++;
+	                printf("\n\t\t\tData berhasil dihapus");
+	                printf("\n\t\t\tKlik terserah untuk lanjut~ ");
+	                getch();
+	                menu();
+	            }
+	            else if(ans == 'n' || ans == 'N')
+	            {
+	                printf("Tekan Enter Untuk Lanjut....");
+	                getch();
+	                system("cls");
+	                menu();
+	            }
+	        }
+	    }
+	} else {
+		gotoxy(31,12);
+    printf("Kode belum ditambahkan");
+	}
+    strcpy(rriwayat[riwayad], "ADMIN MENGHAPUS DATABASE\n");
+    riwayad++;
+    gotoxy(23,11);
+    printf("Kode %s tidak dapat ditemukan, silahkan coba lagi", kode);
+    gotoxy(23,12);
+    printf("Klik terserah untuk lanjut~ ");
+    getch();
+    system("cls");
+    menu();
+}
+
+void cari() {
+	cari:
+	system("cls");
+
+	int i;
+	char kode[100];
+	char pilih;
+	bool ada = false;
+
+	gotoxy(30,3);Sleep(1000);printf("+ ================================================= +");
+    gotoxy(30,5);Sleep(1000);printf("                      DATABASE                      ");
+    gotoxy(30,6);Sleep(1000);printf("         	 === MENCARI ISI DATABASE ===    ");
+    gotoxy(30,8);Sleep(1000);printf("+ ================================================= +");
+    gotoxy(30,9);printf("      Masukkan kode mobil yang ingin dicari :       ");
+    gotoxy(30,10);printf("+ ================================================= +");
+    gotoxy(78,9);scanf("%s", &kode);
+
+	for (i = 0; i < banyakdata; i++) {
+
+		if ((strcmp(dealer[i].kode, kode) == 0)) {
+			gotoxy(32, 12);
+            printf(":: Data Ditemukan ::");
+            gotoxy(32, 13);
+            printf(":: Kode Mobil\t = %s",dealer[i].kode);
+            gotoxy(32, 14);
+            printf(":: Nama Mobil\t = %s",dealer[i].nama_mobil);
+            gotoxy(32, 15);
+            printf(":: Jenis Mobil\t = %s",dealer[i].jenis);
+            gotoxy(32, 16);
+            printf(":: Jumlah Mobil\t = %s", dealer[i].jumlah);
+            gotoxy(32, 17);
+            printf(":: Harga Mobil\t = %s", dealer[i].harga);
+            gotoxy(32, 19);
+			ada = true;
+			strcpy(rriwayat[riwayad], "ADMIN MENCARI DATABASE\n");
+    		riwayad++;
+		    printf("Klik terserah untuk lanjut~ ");
+		    getch();
+			menu();
+		}
+	}
+
+	if (ada == false) {
+	gotoxy(30,12);printf("		Data dengan Kode %s tidak ditemukan", kode);
+	gotoxy(30,14);printf("+ ================================================= +");
+	gotoxy(30,16);printf("		   Ingin mengulang ?(y/n) : ");
+	scanf("%s", &pilih);
+	if (pilih == 'y' || pilih == 'Y') {
+		goto cari;
+	} else {
+		menu();
+	}
+	}
+
+}
